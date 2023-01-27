ba9df54104c85808ada7c36f5cf907bc9a821b0f
Tinesha Levene <26723037+tinesha30@users.noreply.github.com>
2023-01-27 15:28:08 -0500

    Project Setup

---
+++ .scripts/schema.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE RosterSynopsisDB;
+
+CREATE TABLE Artists
+(
+    ArtistId INT identity PRIMARY KEY NOT NULL,
+    ArtistName VARCHAR(500) NOT NULL
+)
+
+CREATE TABLE Rates
+(
+    RateId INT identity PRIMARY KEY NOT NULL,
+    RateAmount VARCHAR(500) NOT NULL,
+    ArtistId INT NOT NULL,
+    FOREIGN KEY (ArtistId) REFERENCES Artists(ArtistId)
+)
+
+CREATE TABLE Streams
+(
+    RateId INT PRIMARY KEY NOT NULL,
+    StreamCount BIGINT NOT NULL DEFAULT(0)
+    FOREIGN KEY (RateId) REFERENCES Rates(RateId)
+)
